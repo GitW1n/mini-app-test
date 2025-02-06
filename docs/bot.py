4e588d535921071848a8cc336bef9987e18e9079
GitW1n <akovabramov1@gmail.com>
2025-02-06 08:23:28 +0300

    Index.html edit

--- docs/bot.py
+++ docs/bot.py
@@ -3,6 +3,7 @@ from telegram.ext import Application, CommandHandler, CallbackQueryHandler, Cont
 from dotenv import load_dotenv
 import os
 
+# Загружаем переменные окружения для токена бота
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 
@@ -10,18 +11,17 @@ application = Application.builder().token(TOKEN).build()
 
 # Функция для отправки стартового сообщения с кнопками
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # Создаем кнопки
+    # Создаем кнопки с web_app для MiniApp
     keyboard = [
-        [InlineKeyboardButton("Перейти в MiniApp", url='https://example.com/miniapp')]  # Указание ссылки на MiniApp
+        [InlineKeyboardButton("Перейти в MiniApp", web_app={'url': 'https://gitw1n.github.io/mini-app-test/'})]  # Ссылка на ваш MiniApp
     ]
 
-    # Используем raw string для пути к изображению
-    image_path = r'C:\Users\micro\VSCodeProjects\Python_cybersec_tests\Telegram_Mini_Apps\docs\photos\logo.jpg'
+    image_path = r'C:\Users\micro\VSCodeProjects\Python_cybersec_tests\Telegram_Mini_Apps\docs\images\logo.jpg'
 
     # Создаем разметку клавиатуры
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    # Отправляем сообщение с кнопками
+    # Отправляем сообщение с изображением и кнопками
     await update.message.reply_photo(
         photo=open(image_path, 'rb'), 
         caption='Выберите виртуальный номер для покупки:', 
@@ -56,18 +56,17 @@ async def buy(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 # Основная функция для запуска бота
 def main() -> None:
-    # Замените 'YOUR_TOKEN' на ваш реальный токен
+    # Строим приложение
     application = Application.builder().token(TOKEN).build()
 
     # Регистрируем обработчики команд
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CallbackQueryHandler(button))
     application.add_handler(CommandHandler("buy_1", buy))
-    application.add_handler(CommandHandler("buy_2", buy))
-    application.add_handler(CommandHandler("buy_3", buy))
 
     # Запускаем бота
     application.run_polling()
 
+# Запуск бота
 if __name__ == '__main__':
     main()
